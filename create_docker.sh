413aa71d69faf40539d210323ea1c67fa2ae895d
timolow <tim@eth0.com>
2024-02-12 17:29:04 +0000

    add host net

--- create_docker.sh
+++ create_docker.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 docker build -t swift .
-docker container run -P \
+docker container run -d -P \
+--user 1111:1111 \
 -v /srv/node:/srv/node \
 -v /etc/swift:/etc/swift \
 -v /var/run/swift:/var/run/swift \
 -v /var/cache/swift:/var/cache/swift \
 -v /var/log/swift:/var/log/swift \
--p 0.0.0.0:6012:6012 \
--p 0.0.0.0:6011:6011 \
--p 0.0.0.0:6010:6010 \
--p 0.0.0.0:8080:8080 \
+--network host \
 -t swift
